0f6cc8fc31b04aeb7d10838849bfa307cb6cb1bc
nicolas.bonzini <nicolas.bonzini@cencosud.com>
2022-11-01 11:24:31 -0300

    Modelos de la base de datos agregados

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -22,5 +26,7 @@ app.get("/api/products/:id", (req, res) => {
 
 app.listen(
   PORT,
-  console.log(`Server run in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server run in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
