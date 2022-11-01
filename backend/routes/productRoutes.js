05933f7833052250c42c3c2357b6e0b15d1778b6
nicolas.bonzini <nicolas.bonzini@cencosud.com>
2022-11-01 12:28:49 -0300

    Rutas agregadas

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
+
+//@descripcion   Trae todos los productos
+//@route         GET /api/products
+//@access        Public
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+//@descripcion   Trae un solo producto
+//@route         GET /api/product
+//@access        Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({
+        message: "Producto no encontrado",
+      });
+    }
+
+    res.json(product);
+  })
+);
+
+export default router;
